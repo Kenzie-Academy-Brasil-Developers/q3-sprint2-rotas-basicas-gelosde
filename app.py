8ec687962e995986a5c32689035b10542065f7a4
gelosde <angelokvieira@gmail.com>
2021-12-16 17:39:48 -0300

    tudo finalizado

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-# Código do dev aqui
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+    return {"data": "Hello Flask!"}
+
+@app.route('/current_datetime',methods=['GET'])
+def current_datetime():
+    timer = datetime.now()
+    timerJob = timer.strftime('%H')
+    msg =""
+    curr_datetime= timer.strftime("%d/%m/%Y %H:%M:%S %p")
+    if int(timerJob[0]) == 0 : msg = "Bom dia!"
+    elif int(timerJob) > 11:
+        if int(timerJob) < 18 : msg = "Boa tarde!" 
+        else : msg = "Boa noite!" 
+    send ={"current_datetime": curr_datetime, "message": msg}
+    return send
